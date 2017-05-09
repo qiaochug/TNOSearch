9f27c23366643bd0875bc9ea27e14378cee060c5
Nova Guo <qiaochug@seas.upenn.edu>
2017-05-09 14:18:45 -0400

    basic functions seems to work, jumps in data generated due to inaccurate approximation of function root, last commit invalid

--- position.py
+++ position.py
@@ -11,7 +11,11 @@ import math
 """
 variables
 """
-e = 0.4 # eccentricity
+a = 40 # semi-axis AU
+e = 0 # eccentricity
+i = 0.5 # inclination
+Omega = 0 # longitude of the ascending node
+omega = 0 #argument of perihelion
 
 """
 Constants
@@ -20,11 +24,21 @@ G = 6.67408e-11 # m^3/kg s gravitational constant
 pi = np.pi
 M = 1.989e30 #kg mass of the sun
 k = 0.017202 # AU day M 0.01720209895 Gaussian gravitational constant
+R = 1 # AU earth-sun distance
 
 """
 global variables
 """
 rad_array = np.zeros(361)
+phi = 0 # angle between the asteroid's position and the ascending node in the
+#orbital plane
+r = 0 #heliocentric distance of the asteroid
+u = 0 # the projected heliocentric distance of an asteroid in the XY plane
+alpha = 0 # angle between the ascending node and a line form the Sun to the 
+#ecliptic plane above or below the asteroid's orbital position
+Lambda_e = 0 #heliocentric longitude (kept stationary temporarily)
+lam = 0 # lambda geometric ecliptic longitude
+beta = 0 # geocentric ecliptic latitude
 
 def period_calc(a):
     """
@@ -41,6 +55,7 @@ def period_calc(a):
 
 def f_RHS(f):
     """
+    helper function, called in f_fill()
     evaluate the right hand side of the time-asteroid_angle equation
     """
     sin = math.sin(f)
@@ -59,8 +74,9 @@ def f_RHS(f):
 
 def f_fill():
     """
+    helper function, user should call it before the first f_calc
     filling the possible solution array with evaluations
-    note possible error: wierod orientation of the ellipse
+    note possible error: negative evaluations at angles larger than pi
     """
     index = 0
    
@@ -71,7 +87,7 @@ def f_fill():
         if RHS < 0:
             RHS = 2*pi + RHS
         rad_array[index] = RHS
-        print(index, deg, rad_array[index])
+        #print(index, deg, rad_array[index])
         index = index + 1 
         
     return
@@ -94,9 +110,101 @@ def f_calc(t, tau):
             diff = abs(LHS - rad_array[index])
     
     return (root/360*2*pi)
+
+def Lambda_e_calc(t):
+    """
+    helper function, called in xyz_calc
+    update earth heliocentric longitude
+    """
+    Le = t/365.25 * 2 *pi
+    return Le
+
+def xyz_calc(f,t):
+    """
+    compute xyz coordinates of the asteroid
+    Return: an array of size 3, x y z
+    """
     
-f_fill()   
-print(f_calc(0.79/2/pi,1))
+    xyz = np.array([0.0,0.0,0.0])
+    
+    global phi
+    global r
+    global u
+    global alpha
+    global Lambda_e
+    
+    phi = omega + f
+    
+    r = a*(1-e**2)/(1+e*math.cos(f))
+    
+    u = r*math.sqrt((math.sin(phi))**2*(math.cos(i))**2+(math.cos(phi))**2)
+    #print("u {:f}".format(u))
+    
+    alpha = math.atan(math.tan(phi)*math.cos(i))
+    #print("alpha {:f}".format(alpha))
+    
+    Lambda_e = Lambda_e_calc(t)
+    #print("lambda_e {:f}".format(Lambda_e))
+    
+    xyz[0] = u*math.cos(Omega + alpha) - R* math.cos(Lambda_e)
+    
+    xyz[1] = u*math.sin(Omega + alpha) - R* math.sin(Lambda_e)
+    
+    xyz[2] = r*math.sin(phi)*math.sin(i)
+    
+    #print("x {:f} y {:f} z {:f}".format(xyz[0], xyz[1], xyz[2]))
+    
+    return xyz
+
+def lambda_beta_calc(xyz):
+    
+    lb = np.array([0.0,0.0])
+    x = xyz[0]
+    y = xyz[1]
+    z = xyz[2]
+    #print("x {:f} y {:f} z {:f}".format(x, y, z))
+    
+    lb[0] = math.atan(y/x)
+    #print(math.atan(y/x))
+    #print("lb[0] {:f}".format(lb[0]))
+    lb[1] = math.atan(z/math.sqrt(x**2+y**2))
+    #print("l {:f} b {:f}".format(lb[0],lb[1]))
+    
+    return lb
+
+
+def generate(startDay, endDay):
+    """
+    print out the lambda beta trace given 
+    a period of time
+    """
+    
+    tau = period_calc(a)
+    
+    f_fill()
+    
+    days = np.arange(startDay, endDay+1)
+    
+    for day in days:
+        f = f_calc(day, tau)
+        #print("day {:f} tau {:f}".format(day, tau))
+        xyz = xyz_calc(f, day)
+        #print("x {:f} y {:f} z {:f}".format(xyz[0], xyz[1], xyz[2]))
+        lb = lambda_beta_calc(xyz)
+        #if lb[0] < 0:
+            #lb[0] = lb[0] + 2*pi
+        print("day {:d} f {:f} lambda {:f} Beta {:f}".format(day, f,lb[0]/2/pi*360, lb[1]/2/pi*360))
+        
+    return
+ 
+generate(0,365)       
+#f_fill()   
+#print(f_calc(0.6,1))
+#xyz = xyz_calc(2,30)
+#print(xyz[0], xyz[1], xyz[2])
+#lb = lambda_beta_calc(xyz)
+#print(lb[0],lb[1])
+
 
 
     
