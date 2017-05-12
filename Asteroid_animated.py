f0863c86836ffdc6d5aa0cd3e68fd8e33a796c23
Nova Guo <qiaochug@seas.upenn.edu>
2017-05-12 15:52:33 -0400

    get accurate alpha by analyzing phi

--- Asteroid_animated.py
+++ Asteroid_animated.py
@@ -16,13 +16,13 @@ from matplotlib import animation
 variables
 """
 a = 40 # semi-axis AU
-e = 0.4 # eccentricity
+e = 0 # eccentricity
 i = 0.4 # inclination
-Omega = np.pi/2 # longitude of the ascending node
-omega = np.pi/2 #argument of perihelion
+Omega = 0 # longitude of the ascending node
+omega = 0 #argument of perihelion
 
 """
-Constants
+Constant
 """
 G = 6.67408e-11 # m^3/kg s gravitational constant
 pi = np.pi
@@ -154,7 +154,38 @@ def xyz_calc(f,t):
     u = r*math.sqrt(((math.sin(phi))**2)*((math.cos(i))**2)+(math.cos(phi))**2)
     #print("u {:f}".format(u))
     
-    alpha = math.atan(math.tan(phi)*math.cos(i))
+    to_be_tan = math.tan(phi)*math.cos(i)
+    #print(to_be_tan)
+    tan_1 = math.atan2(to_be_tan, 1)
+    #print(tan_1)
+    if tan_1 < 0:
+            tan_1 = tan_1 + 2*pi
+    tan_2 = math.atan2(-to_be_tan, -1)
+    #print(tan_2)
+    if tan_2 < 0:
+            tan_2 = tan_2 + 2*pi
+            
+    #print("tan_1 {:f} tan_2 {:f}".format(tan_1, tan_2))
+   
+   
+    
+    if phi >=0 and phi <= pi:
+        if tan_1 >= 0 and tan_1 <= pi:
+            #print("above1")
+            alpha = tan_1
+        else:
+            #print("above2")
+            alpha = tan_2
+    
+    if phi > pi and phi <= 2*pi:
+        if tan_1 > pi and tan_1 <= 2*pi:
+            #print("below1 {:f} {:f}".format(tan_1,tan_2))
+            alpha = tan_1
+        else:
+            #print("below2")
+            alpha = tan_2
+    
+    #alpha = math.atan(to_be_tan)
     #print("alpha {:f}".format(alpha))
     
     Lambda_e = Lambda_e_calc(t)
@@ -180,7 +211,11 @@ def lambda_beta_calc(xyz):
     z = xyz[2]
     #print("x {:f} y {:f} z {:f}".format(x, y, z))
     
-    lb[0] = math.atan(y/x)
+    lb[0] = math.atan2(y,x)
+    if lb[0] < 0:
+        lb[0] = lb[0] + 2*pi
+    #lb[0] = 2*pi - lb[0]                      
+
     #print(math.atan(y/x))
     #print("lb[0] {:f}".format(lb[0]))
     lb[1] = math.atan(z/math.sqrt(x**2+y**2))
@@ -210,14 +245,15 @@ def generate(startDay, endDay):
     for day in days:
         t = day
         f = f_calc()
-        #print("f {:f} day {:f}".format(f,day/365.25*2*pi))
+        #print("f {:f} day {:f}".format(f,day))
         #print("day {:f} tau {:f}".format(day, tau))
         xyz = xyz_calc(f, day)
-        print("day {:d} x {:f} y {:f} z {:f}".format(day, xyz[0], xyz[1], xyz[2]))
+        #print("day {:d} x {:f} y {:f} z {:f}".format(day, xyz[0], xyz[1], xyz[2]))
         lb = lambda_beta_calc(xyz)
 #        if lb[0] < 0:
 #            lb[0] = lb[0] + 2*pi
-        print("day {:d} f {:f} lambda {:f} Beta {:f}".format(day, f,lb[0]/2/pi*360, lb[1]/2/pi*360))
+        
+        #print("day {:d} f {:f} lambda {:f} Beta {:f}".format(day, f,lb[0]/2/pi*360, lb[1]/2/pi*360))
         if lb[1]< largest:
             largest = lb[1]
             largest_day = day
@@ -232,68 +268,68 @@ def generate(startDay, endDay):
     #print(largest_day, largest, negative, f_large, f_negative)  
     return
 
-#generate(0,365)      
-#def animate(startDay, endDay):
+#generate(180,185)      
+def animate(startDay, endDay):
     
-#    plt.close('all')
-#    bound = 365
-#    fig = plt.figure()
-#    ax = plt.axes(xlim=(-bound,bound), ylim=(-bound, bound))
-#    (my_line,) = ax.plot([],[],lw = 2)
-#    (my_point,) = ax.plot([],[],'ro',ms = 9)
-#    
-#    global t
-#    
-#    period_calc(a)
-#    
-#    days = np.arange(startDay, endDay+1)
-#    
-#    x_steps = np.zeros(endDay - startDay +1)
-#    y_steps = np.zeros(endDay - startDay +1)
-#        
-#    count = 0
-#    
-#    for day in days:
-#        t = day
-#        f = f_calc()
-#        #print("day {:f} tau {:f}".format(day, tau))
-#        xyz = xyz_calc(f, day)
-#        #print("x {:f} y {:f} z {:f}".format(xyz[0], xyz[1], xyz[2]))
-#        x = xyz[0]
-#        y = xyz[1]
-#        z = xyz[2]
-#        lb = lambda_beta_calc(xyz)
-##        x_steps[count] = math.sin(lb[0]) * (math.sqrt(x**2+y**2))*10
-##        y_steps[count] = math.sin(lb[1]) * (math.sqrt(x**2+y**2+z**2))*10
-#        x_steps[count] = count
-#        y_steps[count] = count
-#        count = count + 1
-#    
-#    print(x_steps)
-#    print(y_steps)
-#    
-#    def get_step(n,x,y,this_line,this_point):
-#        this_line.set_data(x[:n+1], y[:n+1])
-#        this_point.set_data(x[n], y[n])
-#    
-#    my_movie = animation.FuncAnimation(fig,get_step, frames=endDay - startDay +1,\
-#                                       fargs= (x_steps,y_steps,my_line,my_point))
+    plt.close('all')
+    bound = 365
+    fig = plt.figure()
+    ax = plt.axes(xlim=(-bound,bound), ylim=(-bound, bound))
+    (my_line,) = ax.plot([],[],lw = 2)
+    (my_point,) = ax.plot([],[],'ro',ms = 9)
+    
+    global t
+    
+    period_calc(a)
+    
+    days = np.arange(startDay, endDay+1)
+    
+    x_steps = np.zeros(endDay - startDay +1)
+    y_steps = np.zeros(endDay - startDay +1)
+        
+    count = 0
+    
+    for day in days:
+        t = day
+        f = f_calc()
+        #print("day {:f} tau {:f}".format(day, tau))
+        xyz = xyz_calc(f, day)
+        #print("x {:f} y {:f} z {:f}".format(xyz[0], xyz[1], xyz[2]))
+        x = xyz[0]
+        y = xyz[1]
+        z = xyz[2]
+        lb = lambda_beta_calc(xyz)
+#        x_steps[count] = math.sin(lb[0]) * (math.sqrt(x**2+y**2))*10
+#        y_steps[count] = math.sin(lb[1]) * (math.sqrt(x**2+y**2+z**2))*10
+        x_steps[count] = count
+        y_steps[count] = count
+        count = count + 1
+    
+    print(x_steps)
+    print(y_steps)
+    
+    def get_step(n,x,y,this_line,this_point):
+        this_line.set_data(x[:n+1], y[:n+1])
+        this_point.set_data(x[n], y[n])
+    
+    my_movie = animation.FuncAnimation(fig,get_step, frames=endDay - startDay +1,\
+                                       fargs= (x_steps,y_steps,my_line,my_point))
 
 
 plt.close('all')
 bound = 50
 fig = plt.figure()
-ax = plt.axes(xlim=(-1,5), ylim=(20,30))
+ax = plt.axes(xlim=(0,5), ylim=(-3,3))
 
 (my_line,) = ax.plot([],[],lw = 2)
 (my_point,) = ax.plot([],[],'ro',ms = 3)
 
 period_calc(a)
 
-days = np.arange(0,1000)
+days = np.arange(1,1000)
 
-x_steps = np.zeros(1001)
-y_steps = np.zeros(1001)
+x_steps = np.zeros(1000)
+y_steps = np.zeros(1000)
 
 count  = 0
 for day in days:
@@ -305,11 +341,13 @@ for day in days:
         x = xyz[0]
         y = xyz[1]
         z = xyz[2]
-        #lb = lambda_beta_calc(xyz)
+        lb = lambda_beta_calc(xyz)
 #        x_steps[count] = math.sin(lb[0])* (math.sqrt(x**2+y**2))*20
 #        y_steps[count] = math.sin(lb[1])* (math.sqrt(x**2+y**2+z**2))*20
-        x_steps[count] = y
-        y_steps[count] = x
+        x_steps[count] = lb[0]/2/pi*360
+        y_steps[count] = lb[1]/2/pi*360
+#        x_steps[count] = y
+#        y_steps[count] = x
         #x_steps[count] = count
         #y_steps[count] = count
         count = count + 1
@@ -322,9 +360,9 @@ def get_step(n,x,y,this_line,this_point):
     this_point.set_data(x[n], y[n])
 
 
-my_movie = animation.FuncAnimation(fig,get_step, frames=1001,\
+my_movie = animation.FuncAnimation(fig,get_step, frames=999,\
                                    fargs= (x_steps,y_steps,my_line,my_point))
-my_movie.save('Observe from north pole, facing righthand, x = y, y = x,e = 0.4,i=0.4.mp4',fps = 30)
+my_movie.save('newphiAtane0.4omega0.mp4',fps = 30)
 
 #generate(0,365)
 #f_fill()   
